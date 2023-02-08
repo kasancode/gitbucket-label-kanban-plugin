d603367aa1ab994a138be83f308161bb5440e030
kasancode <kasancode@gmail.com>
2023-02-08 18:21:05 +0900

    Refactoring

--- src/main/scala/io/github/gitbucket/labelkanban/api/ApiIssueKanban.scala
+++ src/main/scala/io/github/gitbucket/labelkanban/api/ApiIssueKanban.scala
@@ -1,39 +1,39 @@
 package io.github.gitbucket.labelkanban.api
 
-import java.util.Date
 import gitbucket.core.api._
-import gitbucket.core.model.{Issue, IssueAssignee, Label, Priority}
-import gitbucket.core.service.IssuesService
+import gitbucket.core.model.{Issue, Label, Priority}
 import gitbucket.core.util.RepositoryName
-import gitbucket.core.view.helpers
-
-import scala.collection.mutable
 
 case class ApiIssueKanban(
-                           userName: String,
-                           issueId: Int,
-                           openedUserName: String,
-                           milestoneId: Int,
-                           priorityId: Int,
-                           assignedUserName: String,
-                           title: String,
-                           content: String,
-                           closed: Boolean,
-                           registeredDate: java.util.Date,
-                           updatedDate: java.util.Date,
-                           isPullRequest: Boolean,
-                           labelNames:List[String],
-                           metrics: Map[String,String],
-                           show: Boolean = false,
-                         )(repositoryName: RepositoryName)
-  extends FieldSerializable {
+  userName: String,
+  issueId: Int,
+  openedUserName: String,
+  milestoneId: Int,
+  priorityId: Int,
+  assignedUserName: String,
+  title: String,
+  content: String,
+  closed: Boolean,
+  registeredDate: java.util.Date,
+  updatedDate: java.util.Date,
+  isPullRequest: Boolean,
+  labelNames: List[String],
+  metrics: Map[String, String],
+  show: Boolean = false
+)(repositoryName: RepositoryName)
+    extends FieldSerializable {
   val htmlUrl = ApiPath(s"/${repositoryName.fullName}/issues/${issueId}")
   val comments_url = ApiPath(s"/api/v3/repos/${repositoryName.fullName}/issues/${issueId}/comments")
 }
 
-
 object ApiIssueKanban {
-  def apply(issue: Issue,assignedUserName: Option[String], labels: List[Label], prefix: String, repositoryName: RepositoryName): ApiIssueKanban =
+  def apply(
+    issue: Issue,
+    assignedUserName: Option[String],
+    labels: List[Label],
+    prefix: String,
+    repositoryName: RepositoryName
+  ): ApiIssueKanban =
     ApiIssueKanban(
       userName = issue.userName,
       issueId = issue.issueId,
@@ -57,7 +57,13 @@ object ApiIssueKanban {
       )
     )(repositoryName)
 
-  def applySummary(issue: Issue, assignedUserName: Option[String], labels: List[Label], prefix: String, priorities: List[Priority]): ApiIssueKanban =
+  def applySummary(
+    issue: Issue,
+    assignedUserName: Option[String],
+    labels: List[Label],
+    prefix: String,
+    priorities: List[Priority]
+  ): ApiIssueKanban =
     ApiIssueKanban(
       userName = issue.userName,
       issueId = issue.issueId,
@@ -81,29 +87,30 @@ object ApiIssueKanban {
       )
     )(RepositoryName(issue.userName, issue.repositoryName))
 
-  def createMetrics(milestoneId: Option[Int],
-                    priorityId: Option[Int],
-                    assignedUserName: Option[String],
-                    labels: List[Label],
-                    prefix: String
-                   ): Map[String, String] = Map(
+  def createMetrics(
+    milestoneId: Option[Int],
+    priorityId: Option[Int],
+    assignedUserName: Option[String],
+    labels: List[Label],
+    prefix: String
+  ): Map[String, String] = Map(
     "None" -> "0",
     "Label:" + prefix -> labels.find(_.labelName.startsWith(prefix)).map(_.labelId).getOrElse(0).toString,
     "Milestones" -> milestoneId.getOrElse(0).toString,
     "Priorities" -> priorityId.getOrElse(0).toString,
     "Assignees" -> (assignedUserName match {
-      case Some(s) if s.length > 0 => s
+      case Some(s) if s.nonEmpty => s
       case _ => "-"
     })
   )
 
   def createSummaryMetrics(
-                            labels: List[Label],
-                            prefix: String,
-                            priority: Option[Priority],
-                            assignedUserName: Option[String],
-                            repository: String
-                          ): Map[String, String] = Map(
+    labels: List[Label],
+    prefix: String,
+    priority: Option[Priority],
+    assignedUserName: Option[String],
+    repository: String
+  ): Map[String, String] = Map(
     "None" -> "-",
     "Label:" + prefix -> labels.find(_.labelName.startsWith(prefix)).map(_.labelName).getOrElse("-"),
     "Priorities" -> priority.map(_.priorityName).getOrElse("-"),
